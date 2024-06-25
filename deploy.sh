939d84ef99823bce86f721975284e29089cb0c8a
Arman <arman.dogru@gmail.com>
2024-06-25 14:13:01 -0400

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Ensure we're using a compatible Python version
-pyenv install 3.8.12
-pyenv global 3.8.12
-
 # Upgrade pip and setuptools
 pip install --upgrade pip setuptools
 
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
+
+# Install Netlify CLI
+npm install -g netlify-cli
 
 # Package the application
 echo "Packaging FPGA Development Suite..."
